82f58d0fa75b4cff0da4afcc4e2be8b6f257f3f0
Shubham Ivane <ivaneshubham@gmail.com>
2019-10-13 03:02:24 +0530

    new branch

--- src/main/java/com/truecaller/clone/user/UserRepository.java
+++ src/main/java/com/truecaller/clone/user/UserRepository.java
@@ -3,5 +3,7 @@ package com.truecaller.clone.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String >{
+	
 	public User findByPhoneNoAndPassword(String phoneNo, String password);
+	
 }
